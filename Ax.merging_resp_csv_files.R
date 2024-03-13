5f068797ddcefe17dbb7d5dad2a7366cb4c6e861
schmidte10 <elliott.a.schmidt@gmail.com>
2024-03-13 10:44:38 +1000

    updated table export for resp data to .txt and .csv files

--- Ax.merging_resp_csv_files.R
+++ Ax.merging_resp_csv_files.R
@@ -2,5 +2,6 @@ setwd("./resp_extracted_rates")
 file_names <- dir()
 lat_resp_dat <- do.call(rbind,lapply(file_names, read.csv))
 save(lat_resp_dat, file="../import_files/lat_resp_dat.Rda") 
-save(lat_resp_dat, file="../import_files/lat_resp_dat.txt") 
-save(lat_resp_dat, file="../excel_files/lat_resp_dat.csv") 
+write.table(lat_resp_dat, file="../import_files/lat_resp_dat.txt", sep=" ") 
+write.csv(lat_resp_dat, file="../excel_files/lat_resp_dat.csv", row.names = FALSE) 
+
